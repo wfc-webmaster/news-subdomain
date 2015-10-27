465044226a858aa41c75a5730be40a682f3f9985
wedesignstudios <deelden@gmail.com>
2015-10-27 16:37:31 -0500

    New footer

---
+++ custom_code/footer/footer-ut.php
@@ -0,0 +1,7 @@
+<div id="footer-ut-container">
+	<div id="footer-ut-wrap" class="mobile-padding">
+		<div id="footer-ut-logo">
+			<img src="//localhost:8888/wildflower_news/wp-content/uploads/2015/07/knockout_university_primary.svg">
+		</div>
+	</div>
+</div>
