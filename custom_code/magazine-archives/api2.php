78eb9fb2b0c8237742b39063c4bff097efe6dfea
Elden <webmaster@wildflower.org>
2015-11-13 16:18:29 -0600

    Mag archive display fixes

--- custom_code/magazine-archives/api2.php
+++ custom_code/magazine-archives/api2.php
@@ -4,42 +4,67 @@
         $db_name  = 'wfc_webdev';		
         $hostname = 'mysqldb.its.utexas.edu';		
         $username = 'wildflow';		
-        $password = 'jwfP2xHJ39jT2qfN';		
+        $password = 'pKMhU27eWTaFKPMS';		
 		
         // connect to the database		
-        $dbh = new PDO("mysql:host=$hostname;dbname=$db_name;charset=utf8", $username, $password);	
-        
-        // a query get all the records from the users table		
-        //$sql = 'SELECT * FROM app_pastissues WHERE id>=1';              
-        $sql = '
-                SELECT GROUP_CONCAT(name SEPARATOR "|") as title
+        $dbh = new PDO("mysql:host=$hostname;dbname=$db_name;charset=utf8", $username, $password);
+
+        $sql_1 = "
+                SELECT DISTINCT issue, volume, number
                 FROM app_pastissues
-                WHERE volume=31 AND number=1
-                ';		
-		
-        // use prepared statements, even if not strictly required is good practice		
-        $stmt = $dbh -> prepare( $sql );		
-		
-        // execute the query		
-        $stmt -> execute();		
-		
-        // fetch the results into an array		
-        $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+                ORDER BY volume DESC, number ASC
+                ";
 
-        $mag = $result[0]['col'];
+        // use prepared statements, even if not strictly required is good practice              
+        $stmt_1 = $dbh -> prepare( $sql_1 );              
+                
+        // execute the query            
+        $stmt_1 -> execute();             
+                
+        // fetch the results into an array              
+        $result_1 = $stmt_1 -> fetchAll(PDO::FETCH_ASSOC);
 
-        $magYeah = explode("|", $mag);
+        //print_r($result_1);
 
-        print_r($magYeah);
-        // var_dump($result);
-        //var_export($result);
-		
-        // convert to json		
-        $json = json_encode($result);		
-		
-		
-        // echo the json string		
-        echo $json;
+        foreach ($result_1 as $mag_issue => $value) {
+                $issue = $value['issue'];
+
+        
+                //echo $issue . '<br />';
+
+                $sql_2 = "
+                        SELECT DISTINCT name, content
+                        FROM app_pastissues
+                        WHERE issue = '$issue'
+                        ORDER BY volume DESC, id ASC
+                        ";              
+                        
+
+                // use prepared statements, even if not strictly required is good practice              
+                $stmt_2 = $dbh -> prepare( $sql_2 );            
+                        
+                // execute the query            
+                $stmt_2 -> execute();           
+                        
+                // fetch the results into an array              
+                $result_2 = $stmt_2 -> fetchAll(PDO::FETCH_ASSOC);
+
+                $mag_content['stories'] = $result_2;
+
+                $mag_complete = $value+$mag_content;
+                                               
+                //print_r($mag_complete);
+                //var_dump($result_2);
+                //var_export($result_2);
+                        
+                // convert to json              
+                $json = json_encode($mag_complete);         
+                        
+                        
+                // echo the json string         
+                echo $json;
+        
+                }
         
 
 ?>
--- custom_code/magazine-archives/controllers.js
+++ custom_code/magazine-archives/controllers.js
@@ -2,7 +2,7 @@ var magazineControllers = angular.module("magazineControllers", ['ngSanitize']);
 
 magazineControllers.controller("SummaryController", function($scope, $http, $timeout){
 	$http.get('http://localhost:8888/wildflower_news/wp-content/themes/wildflowercenter/custom_code/magazine-archives/api.php').success(function(data) {
-            // here the data from the api is assigned to a variable named users
+             // here the data from the api is assigned to a variable named users             
         $scope.magazineissues = data;
         $scope.orderMagazineIssues = 'id';
         $scope.direction = 'reverse';
